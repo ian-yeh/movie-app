14de2c35cf3e0501bf6b5ec2b18bbafafccbfa2e
Ian Yeh <its.ian.y07@gmail.com>
2025-04-22 22:06:18 -0400

    finishing movie page, tab nav

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+
+import { icons } from '@/constants/icons';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="foex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} tintColor="#fff" className="size-10" />
+        <Text className="text-light-200 text-base">Profile</Text>
+       </View>
     </View>
   )
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+
+import { icons } from '@/constants/icons';
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="foex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} tintColor="#fff" className="size-10" />
+        <Text className="text-light-200 text-base">Saved</Text>
+       </View>
     </View>
   )
 }
